cd43d0d81a897d6cb857f04ecfa5dd9ff72a7ce0
Juan Hurtado <juanhurtado2994@gmail.com>
2025-01-09 17:20:58 -0500

    lambda bucket s3

--- src/main/java/org/lambda/LambdaFunctionHandler.java
+++ src/main/java/org/lambda/LambdaFunctionHandler.java
@@ -14,50 +14,42 @@ import java.nio.file.Paths;
 public class LambdaFunctionHandler implements RequestHandler<Object, String> {
 
     // Región de AWS
-    private static final Region REGION = Region.US_EAST_1; // Cambia la región según sea necesario
-    private static final String BUCKET_NAME = "automationbuckets3"; // Nombre de tu bucket S3
-    private static final String FILE_NAME = "test.txt"; // Nombre del archivo en S3
+    private static final Region REGION = Region.US_EAST_1;
+    private static final String BUCKET_NAME = "automationbuckets3";
+    private static final String FILE_NAME = "test.txt";
     private static final String FILE_CONTENT = "{"
             + "\"name\": \"John\","
             + "\"age\": 30,"
             + "\"city\": \"New York\""
-            + "}";// Contenido del archivo
+            + "}";
 
     @Override
     public String handleRequest(Object o, Context context) {
-        // Configuración del cliente S3
+
         S3Client s3Client = S3Client.builder()
                 .region(REGION)
                 .build();
 
-        // Llamar a la función que sube el archivo a S3
         uploadFileToS3(s3Client);
 
-        // Cerrar el cliente S3
         s3Client.close();
 
-        // Devolver mensaje de éxito
         return "Archivo subido exitosamente a S3.";
     }
 
     private void uploadFileToS3(S3Client s3Client) {
         try {
-            // Ruta temporal en Lambda (/tmp) para almacenar el archivo
             Path filePath = Paths.get("/tmp", FILE_NAME);
 
-            // Escribir el contenido del archivo en el directorio temporal
             Files.write(filePath, FILE_CONTENT.getBytes());
 
-            // Crear solicitud para subir el archivo a S3
             PutObjectRequest putObjectRequest = PutObjectRequest.builder()
                     .bucket(BUCKET_NAME)
                     .key(FILE_NAME)
                     .build();
 
-            // Subir el archivo a S3
             PutObjectResponse response = s3Client.putObject(putObjectRequest, filePath);
 
-            // Confirmación de que el archivo fue subido correctamente
             System.out.println("Archivo subido exitosamente con ETag: " + response.eTag());
 
         } catch (Exception e) {
